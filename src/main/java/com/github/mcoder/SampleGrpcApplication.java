8b899d13012a2ac2ef7348f78c746319541ab17f
Mohit Pant <MOHITPAN@amdocs.com>
2020-08-12 19:39:38 +0300

    updated code to work with spring

--- src/main/java/com/github/mcoder/SampleGrpcApplication.java
+++ src/main/java/com/github/mcoder/SampleGrpcApplication.java
@@ -3,19 +3,51 @@ package com.github.mcoder;
 import com.github.mcoder.service.GreetingServiceImpl;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@SpringBootApplication
 public class SampleGrpcApplication {
 
-    public static void main(String[] args) throws Exception {
 
-        Server server = ServerBuilder.forPort(9091)
+    @Autowired
+    private Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleGrpcApplication.class, args);
+    }
+
+
+    @PostConstruct
+    public void startGrpcServer() {
+        String port = environment.getProperty("grpc.server.port", "9091");
+        Server server = ServerBuilder.forPort(Integer.valueOf(port))
                 .addService(new GreetingServiceImpl())
                 .build();
 
-        server.start();
-        System.out.println("Server started");
+        Runtime.getRuntime().addShutdownHook(new Thread(
+                () -> {
+                    log.info("shutting down server");
+                    server.shutdown();
+                }
+        ));
+
+        try {
+            server.start();
+            log.info("server started on port : {}", port);
+            server.awaitTermination();
+        }
+        catch (Exception ex) {
 //        don't exit main thread , wait until server is terminated
-        server.awaitTermination();
+            log.error("error while starting server : {}", ex);
+        }
+
 
     }
 }
